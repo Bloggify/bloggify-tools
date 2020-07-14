bcd41adbb7ebe428497dd14fd9d22e5b9e50beb7
Ionică Bizău <bizauionica@gmail.com>
2020-07-14 16:15:29 +0300

    add the migration tools

--- bin/bloggify-tools.js
+++ bin/bloggify-tools.js
@@ -11,74 +11,148 @@ const Tilda = require("tilda")
     ;
 
 new Tilda(`${__dirname}/../package.json`).main(action => {
-    inquirer.prompt([
-	{
-	    type: "list",
-	    name: "main",
-	    message: "What do you want to do?",
-	    choices: [
-                {
-                    name: "Create a blog for me"
-                  , value: "personal-blog"
-                }
-              , {
-                    name: "Create a blog for somebody else"
-                  , value: "blog"
-                }
-              , {
-                    name: "Create a custom application"
-                  , value: "custom-app"
-                }
-              , {
-                    name: "Start my app"
-                  , value: "start"
-                  , disabled: true
-                }
-              , {
-                    name: "Install a plugin"
-                  , value: "install-plugin"
-                  , disabled: true
-                }
-              , {
-                    name: "Uninstall a plugin"
-                  , value: "uninstall-plugin"
-                  , disabled: true
-                }
-	    ]
-	}
-    ]).then(function (answers) {
-        switch (answers.main) {
+    const MAIN_MENU = {
+        title: "What do you want to do?",
+        choices: [
+            {
+                name: "New Application"
+              , value: "new-application"
+              , choices: [
+                    {
+                        name: "Create a blog for me"
+                      , value: "personal-blog"
+                    }
+                  , {
+                        name: "Create a blog for somebody else"
+                      , value: "blog"
+                    }
+                  , {
+                        name: "Create a custom application"
+                      , value: "custom-app"
+                    }
+                  , {
+                        name: "« Back",
+                        value: "_back"
+                    }
+                ]
+            },
+            {
+                name: "Application Database Migrations"
+              , value: "application-database-migrations"
+              , choices: [
+                    {
+                        name: "Run migrations"
+                      , value: "run-migrations"
+                    }
+                  , {
+                        name: "Create migration"
+                      , value: "create-migration"
+                    }
+                  , {
+                        name: "« Back",
+                        value: "_back"
+                    }
+                ]
+            },
+            {
+                name: "Application Functions"
+              , value: "application-functions"
+              , choices: [
+                    {
+                        name: "Start my app"
+                      , value: "start"
+                      , disabled: true
+                    }
+                  , {
+                        name: "Install a plugin"
+                      , value: "install-plugin"
+                      , disabled: true
+                    }
+                  , {
+                        name: "Uninstall a plugin"
+                      , value: "uninstall-plugin"
+                      , disabled: true
+                    }
+                  , {
+                        name: "« Back",
+                        value: "_back"
+                    }
+                ]
+            }
+        ]
+    }
+
+    const processMenu = async () => {
+
+        let parent  = MAIN_MENU
+
+        do {
+            parent.choices = parent.choices || []
+            const currentMenu = {
+                type: "list",
+                name: "main",
+                message: parent.title || parent.name,
+                choices: parent.choices.map(c => {
+                    return {
+                        name: c.name,
+                        value: c.value
+                    }
+                })
+            }
+
+            const choiceMap = parent.choices.reduce((acc, c) => {
+                acc[c.value] = c
+                return acc
+            }, {})
+
+            const answer = (await inquirer.prompt([currentMenu])).main
+            const answerMenu = choiceMap[answer]
+            answerMenu.choices = answerMenu.choices || []
+
+            if (answerMenu.choices.length) {
+                answerMenu._parent = parent
+                parent = answerMenu
+            } else if (answer === "_back") {
+                parent = parent._parent
+            } else {
+                return answer
+            }
+        } while (true)
+    }
+
+    processMenu().then(answer => {
+        switch (answer) {
             case "blog":
             case "personal-blog":
             case "custom-app":
                 log("So, you want to create a new Bloggify app! Cool. Please answer the following questions. If you're happy with the defaults (the one in parentheses), just press the return key.", "info");
                 fullname().then(userFullName=> {
                     let questions = [
-                        answers.main !== "custom-app" ? {
+                        answer !== "custom-app" ? {
                             type: "input",
                             name: "full_name",
                             default: () => userFullName,
-                            message: answers.main === "personal-blog" ? "Your name" : "Your friend's name"
+                            message: answer === "personal-blog" ? "Your name" : "Your friend's name"
                         } : null,
                         {
                             type: "input",
                             name: "git_url",
-                            default: () => answers.main === "custom-app" ? "https://github.com/BloggifyTutorials/custom-app" : "https://github.com/Bloggify/bloggify-quick-start.git",
+                            default: () => answer === "custom-app" ? "https://github.com/BloggifyTutorials/custom-app" : "https://github.com/Bloggify/bloggify-quick-start.git",
                             message: "Application Template"
                         },
                         {
                             type: "input",
                             name: "title",
-                            default: answs => /blog/.test(answers.main) ? answs.full_name : "Custom App",
+                            default: answs => /blog/.test(answer) ? answs.full_name : "Custom App",
                             message: "Website title"
                         },
                         {
                             type: "input",
                             name: "description",
                             message: "Website description",
-                            default: answs => `${/blog/.test(answers.main) ? answs.full_name + "'s Blog" : "My fancy Bloggify app."}`
+                            default: answs => `${/blog/.test(answer) ? answs.full_name + "'s Blog" : "My fancy Bloggify app."}`
                         },
-                        answers.main !== "custom-app" ? {
+                        answer !== "custom-app" ? {
                             type: "input",
                             name: "theme",
                             message: "Website theme",
@@ -105,6 +179,22 @@ new Tilda(`${__dirname}/../package.json`).main(action => {
             case "install-plugin":
                 break;
             case "uninstall-plugin":
+                break;
+
+            // Migrations
+            case "run-migrations":
+                Bloggify.runMigrations()
+                break;
+
+            // Migrations
+            case "create-migration":
+                inquirer.prompt([{
+                    type: "input",
+                    name: "migration_name",
+                    message: "Migration name"
+                }]).then(({ migration_name }) => {
+                    Bloggify.createMigration(migration_name)
+                })
                 break;
         }
     });
